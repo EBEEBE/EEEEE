95c3ffb8ec8d65530f7cfc5b88c5be4fcb83ce41
EBEEBE <wdowiak@student.agh.edu.pl>
2022-11-03 09:14:40 -0700

    Using test-value

--- WebAppHW/Program.cs
+++ WebAppHW/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello from CI/CD");
-
+var configuration = (IConfiguration)app.Service(typeof(IConfiguration));
+app.MapGet("/", () => $"Hello world! Value: {configuration.GetSection("test").Value}");
 app.Run();
